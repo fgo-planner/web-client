613211e75d5fa010d147b62a54ca655c66cf22db
Alvin Quach <alvinquach91@gmail.com>
2021-01-23 15:43:01 -0800

    refactor: reorganized directories

--- src/modules/authenticated/routes/master-items.route.tsx
+++ src/modules/authenticated/routes/master-items.route.tsx
@@ -1,6 +1,6 @@
 import { RouteComponent } from 'internal';
 import React, { ReactNode } from 'react';
-import { MasterItems } from '../components/master/master-items/master-items.component';
+import { MasterItems } from '../components/master/item/master-items.component';
 
 export class MasterItemsRoute extends RouteComponent {
 

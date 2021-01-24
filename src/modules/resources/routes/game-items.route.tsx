74f8dc7b78527f1720d9368c463efeda03fa4e99
Alvin Quach <alvinquach91@gmail.com>
2021-01-23 23:13:59 -0800

    populate item and servant resource routes with placeholder data

--- src/modules/resources/routes/game-items.route.tsx
+++ src/modules/resources/routes/game-items.route.tsx
@@ -1,14 +1,34 @@
+import { GameItem } from 'data';
 import { RouteComponent } from 'internal';
 import React, { Fragment, ReactNode } from 'react';
+import { GameItemService } from 'services';
+import { Container as Injectables } from 'typedi';
 
 export class GameItemsRoute extends RouteComponent {
 
+    private _gameItemService = Injectables.get(GameItemService);
+
+    private _data: ReadonlyArray<Readonly<GameItem>> = [];
+
+    async componentDidMount() {
+        this._data = await this._gameItemService.getItems();
+        this.forceUpdate();
+    }
 
     render(): ReactNode {
         return (
             <Fragment>
                 <div>ITEMS</div>
+                {this._data.map(this._renderItem)}
             </Fragment>
+        );
+    }
+
+    private _renderItem(item: Readonly<GameItem>, key: number) {
+        return (
+            <div key={key}>
+                {item.name}
+            </div>
         );
     }
 
--- src/modules/resources/routes/game-servants.route.tsx
+++ src/modules/resources/routes/game-servants.route.tsx
@@ -1,11 +1,34 @@
+import { GameServant } from 'data';
 import { RouteComponent } from 'internal';
-import React, { ReactNode } from 'react';
+import React, { Fragment, ReactNode } from 'react';
+import { GameServantService } from 'services';
+import { Container as Injectables } from 'typedi';
 
 export class GameServantsRoute extends RouteComponent {
 
+    private _gameServantService = Injectables.get(GameServantService);
+
+    private _data: ReadonlyArray<Readonly<GameServant>> = [];
+
+    async componentDidMount() {
+        this._data = await this._gameServantService.getServants();
+        this.forceUpdate();
+    }
+
     render(): ReactNode {
         return (
-            <div>SERVANTS</div>
+            <Fragment>
+                <div>SERVANTS</div>
+                {this._data.map(this._renderServant)}
+            </Fragment>
+        );
+    }
+
+    private _renderServant(servant: Readonly<GameServant>, key: number) {
+        return (
+            <div key={key}>
+                {servant.collectionNo} {servant.name}
+            </div>
         );
     }
 

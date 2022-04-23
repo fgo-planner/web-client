8d774cba9c961da02c1b03b534e8aac3a8dfaed8
Alvin Quach <alvinquach91@gmail.com>
2022-04-23 02:02:08 -0700

    removed unused component

--- src/components/plan/requirements/table/plan-requirements-table-row.component.tsx
+++ src/components/plan/requirements/table/plan-requirements-table-row.component.tsx
@@ -1,5 +1,3 @@
-import { FilteringStyledOptions } from '@mui/styled-engine';
-import { styled } from '@mui/system';
 import clsx from 'clsx';
 import React, { CSSProperties, ReactNode, useMemo } from 'react';
 import { PlanRequirementsTableOptionsInternal } from './plan-requirements-table-options-internal.type';
@@ -44,41 +42,4 @@ export const PlanRequirementsTableRow = React.memo((props: Props) => {
         </div>
     );
 
-});
-
-
-const StyleOptions = {
-    name: StyleClassPrefix,
-    slot: 'root',
-    skipSx: true,
-    shouldForwardProp: ((prop: string) => (
-        prop !== 'borderTop' &&
-        prop !== 'borderBottom'
-    )) as unknown
-} as FilteringStyledOptions<Props>;
-
-export const PlanRequirementsTableRowOld = styled('div', StyleOptions)<Props>(props => {
-    const {
-        borderBottom,
-        borderTop,
-        theme
-    } = props;
-
-    const style = {
-        display: 'flex',
-        width: 'fit-content'
-    } as CSSProperties;
-
-    if (borderTop) {
-        style.borderTopWidth = 1;
-        style.borderTopStyle = 'solid';
-        style.borderTopColor = theme.palette.divider;
-    }
-    if (borderBottom) {
-        style.borderBottomWidth = 1;
-        style.borderBottomStyle = 'solid';
-        style.borderBottomColor = theme.palette.divider;
-    }
-
-    return style as any;
 });

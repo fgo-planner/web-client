ee24f54ac5efe0be8261a91750e0e694bfe5dd63
Alvin Quach <alvinquach91@gmail.com>
2022-07-17 22:47:57 -0700

    added new mats

--- src/constants/game-item.constants.ts
+++ src/constants/game-item.constants.ts
@@ -52,16 +52,18 @@ export class GameItemConstants {
      * in the Item List in-game menu.
      */
     static readonly BronzeEnhancementMaterials: ReadonlyArray<number> = [
-        6503, // Proof
-        6516, // Bone
-        6512, // Fang
-        6505, // Dust
-        6522, // Chain
-        6527, // Stinger
-        6530, // Fluid
-        6533, // Stake
-        6534, // Bullet
-        6549  // Bell of Amnesty
+        6503,  // Proof
+        6516,  // Bone
+        6512,  // Fang
+        6505,  // Dust
+        6522,  // Chain
+        6527,  // Stinger
+        6530,  // Fluid
+        6533,  // Stake
+        6534,  // Bullet
+        6549,  // Bell of Amnesty
+        6551,  // Blade
+        6552   // Ashes
     ];
 
     /**
@@ -69,27 +71,27 @@ export class GameItemConstants {
      * in the Item List in-game menu.
      */
     static readonly SilverEnhancementMaterials: ReadonlyArray<number> = [
-        6502, // Seed
-        6508, // Lantern
-        6515, // Crystal
-        6509, // Jewel
-        6501, // Feather
-        6510, // Gear
-        6511, // Page
-        6514, // Baby
-        6513, // Horseshoe
-        6524, // Medal
-        6526, // Shell
-        6532, // Magatama
-        6535, // Frost
-        6537, // Ring
-        6536, // Steel
-        6538, // Bell
-        6541, // Arrowhead
-        6543, // Crown
-        6545, // Vein
-        6547, // Rainbow Yarn
-        6550  // Scales of Fantasies
+        6502,  // Seed
+        6508,  // Lantern
+        6515,  // Crystal
+        6509,  // Jewel
+        6501,  // Feather
+        6510,  // Gear
+        6511,  // Page
+        6514,  // Baby
+        6513,  // Horseshoe
+        6524,  // Medal
+        6526,  // Shell
+        6532,  // Magatama
+        6535,  // Frost
+        6537,  // Ring
+        6536,  // Steel
+        6538,  // Bell
+        6541,  // Arrowhead
+        6543,  // Crown
+        6545,  // Vein
+        6547,  // Rainbow Yarn
+        6550   // Scales of Fantasies
     ];
 
     /**
@@ -97,29 +99,29 @@ export class GameItemConstants {
      * the Item List in-game menu.
      */
     static readonly GoldEnhancementMaterials: ReadonlyArray<number> = [
-        6507, // Claw
-        6517, // Heart
-        6506, // Scale
-        6518, // Root
-        6519, // Horn
-        6520, // Tearstone
-        6521, // Grease
-        6523, // Lamp
-        6525, // Scarab
-        6528, // Lanugo
-        6529, // Gallstone
-        6531, // Wine
-        6539, // Core
-        6540, // Mirror
-        6542, // Egg
-        6544, // Shard
-        6546, // Fruit
+        6507,  // Claw
+        6517,  // Heart
+        6506,  // Scale
+        6518,  // Root
+        6519,  // Horn
+        6520,  // Tearstone
+        6521,  // Grease
+        6523,  // Lamp
+        6525,  // Scarab
+        6528,  // Lanugo
+        6529,  // Gallstone
+        6531,  // Wine
+        6539,  // Core
+        6540,  // Mirror
+        6542,  // Egg
+        6544,  // Shard
+        6546,  // Fruit
         6548  // Demonic Flame
     ];
 
     static readonly OtherEnhancementMaterials: ReadonlyArray<number> = [
-        6999, // Lore
-        7999, // Grail
+        6999,  // Lore
+        7999   // Grail
     ];
 
     static readonly MaxItemQuantity = 2000000000;

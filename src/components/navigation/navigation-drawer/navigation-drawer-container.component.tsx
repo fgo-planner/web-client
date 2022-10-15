380c044d152b98416121eb55a099ba415c2446ca
Alvin Quach <alvinquach91@gmail.com>
2022-10-15 14:53:17 -0700

    updated user settings route
    - added link in drawer
    - added link to mater account settings

--- src/components/navigation/navigation-drawer/navigation-drawer-container.component.tsx
+++ src/components/navigation/navigation-drawer/navigation-drawer-container.component.tsx
@@ -1,6 +1,6 @@
 import { Nullable } from '@fgo-planner/common-core';
 import { MasterAccount } from '@fgo-planner/data-core';
-import { BusinessCenter as BusinessCenterIcon, BusinessCenterOutlined as BusinessCenterOutlinedIcon, CalendarMonth as CalendarMonthIcon, CalendarMonthOutlined as CalendarMonthOutlinedIcon, Category as CategoryIcon, CategoryOutlined as CategoryOutlinedIcon, Dashboard as DashboardIcon, DashboardOutlined as DashboardOutlinedIcon, Group as GroupIcon, GroupOutlined as GroupOutlinedIcon, Home as HomeIcon, HomeOutlined as HomeOutlinedIcon, Login as LoginIcon, Logout as LogoutIcon, MusicNote as MusicNoteIcon, MusicNoteOutlined as MusicNoteOutlinedIcon, PeopleAlt as PeopleAltIcon, PeopleAltOutlined as PeopleAltOutlinedIcon, Stadium as StadiumIcon, StadiumOutlined as StadiumOutlinedIcon, TheaterComedy as TheaterComedyIcon, TheaterComedyOutlined as TheaterComedyOutlinedIcon } from '@mui/icons-material';
+import { BusinessCenter as BusinessCenterIcon, BusinessCenterOutlined as BusinessCenterOutlinedIcon, CalendarMonth as CalendarMonthIcon, CalendarMonthOutlined as CalendarMonthOutlinedIcon, Category as CategoryIcon, CategoryOutlined as CategoryOutlinedIcon, Dashboard as DashboardIcon, DashboardOutlined as DashboardOutlinedIcon, Group as GroupIcon, GroupOutlined as GroupOutlinedIcon, Home as HomeIcon, HomeOutlined as HomeOutlinedIcon, Login as LoginIcon, Logout as LogoutIcon, MusicNote as MusicNoteIcon, MusicNoteOutlined as MusicNoteOutlinedIcon, PeopleAlt as PeopleAltIcon, PeopleAltOutlined as PeopleAltOutlinedIcon, Settings, SettingsOutlined, Stadium as StadiumIcon, StadiumOutlined as StadiumOutlinedIcon, TheaterComedy as TheaterComedyIcon, TheaterComedyOutlined as TheaterComedyOutlinedIcon } from '@mui/icons-material';
 import { Theme } from '@mui/material';
 import { Box, SystemStyleObject } from '@mui/system';
 import React, { PropsWithChildren, useCallback, useEffect, useMemo, useState } from 'react';
@@ -226,6 +226,13 @@ export const NavigationDrawerContainer = React.memo((props: Props) => {
             sections.push({
                 key: 'logout',
                 items: [
+                    {
+                        key: 'user-settings',
+                        icon: SettingsOutlined,
+                        activeIcon: Settings,
+                        label: 'Settings',
+                        route: '/user/settings'
+                    },
                     {
                         key: 'logout',
                         icon: LogoutIcon,
--- src/modules/authenticated/routes/user-settings.route.tsx
+++ src/modules/authenticated/routes/user-settings.route.tsx
@@ -7,6 +7,14 @@ export const UserSettingsRoute = React.memo(() => (
     <Fragment>
         <PageTitle>User Settings</PageTitle>
         <div className='p-4'>
+            <div>
+                <p>
+                    This page contains the global settings for your entire user account. 
+                </p>
+                <p>
+                    Click <Link to='../master/settings'>here</Link> to manage the settings for the currently active master account instead.
+                </p>
+            </div>
             <Button component={Link} to='theme'>
                 Edit Theme
             </Button>
